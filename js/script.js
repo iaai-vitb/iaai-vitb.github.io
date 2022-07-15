98048975abc4742edaf2e58049ff43694a33812a
rudransh khandelwal <rudranshkhandelwal123@gmail.com>
2022-07-15 12:32:17 +0530

    updates

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@
         hour = minute * 60,
         day = hour * 24;
 
-    birthday = "8/24/22 10:00:00"
+    birthday = "12/15/22 10:00:00"
 
 
     const countDown = new Date(birthday).getTime(),
